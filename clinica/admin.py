5050d9948784cf09497503719905c5995c0e59af
Jumara <jumara.spimenta@gmail.com>
2024-10-22 19:27:42 -0400

    inclui modelo fisioterapeuta_servico no djangoadmin

--- clinica/admin.py
+++ clinica/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import User, Servico, CasoClinico
+from .models import User, Servico, CasoClinico, ServicoFisioterapeuta
 
 # Registra o modelo de Usuário
 @admin.register(User)
@@ -22,4 +22,10 @@ class ServicoAdmin(admin.ModelAdmin):
 class CasoClinicoAdmin(admin.ModelAdmin):
     list_display = ('descricao', 'paciente', 'servico_fisioterapeuta', 'situacao')
     search_fields = ('descricao',)
+
+# Registra o modelo de Fisioterapeuta_Servico
+@admin.register(ServicoFisioterapeuta)
+class ServicoFisioterapeutaAdmin(admin.ModelAdmin):
+    list_display = ('fisioterapeuta', 'servico')
+    search_fields = ('fisioterapeuta', 'servico')
 
